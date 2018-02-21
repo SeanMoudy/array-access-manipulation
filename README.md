221189ffc22fc2279f467c486fc0dda1270db63f
matt-winzer <matt.winzer@gmail.com>
2018-02-21 14:35:37 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Array Access and Manipulation
+
+## Objectives
+
+- Access/manipulate data at the back of an array
+- Access/manipulate data at the front of an array
+- Use nested for loops to access data in an array of arrays
+
+## Notes
---
+++ app.js
@@ -0,0 +1,17 @@
+var namesArray = ['matt', 'kim', 'kyle', 'dan', 'james']
+var numsArray = [5, 12, 54, 13, 92]
+
+var arrayOfArrays = [[1, 2, 3], ["a", "b", "c"], ["i", "ii", "iii"]]
+
+
+// Remove 'james' from the back of the namesArray and store it in a variable
+
+// Add your name to the back of the namesArray
+
+// Remove the first number from the front of the numsArray
+
+// Add your favorite number to the front of the numsArray
+
+// Write a function that takes a name (string), and searches the names array and finds the index of th
+
+// Console.log each value from the arrayOfArrays individually (nested for loops)
